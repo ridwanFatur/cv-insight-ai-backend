ac19694459cad8f0e6ef35cb0eb229d7d01eac11
ridwanFatur <ridwan.faturrahnman.godjali@gmail.com>
2026-03-03 17:24:38 +0700

    Update Models

--- models/user.py
+++ models/user.py
@@ -1,5 +1,8 @@
 from sqlalchemy import Column, Integer, String
 from db.database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from sqlalchemy import DateTime
 
 
 class User(Base):
@@ -8,3 +11,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    tokens = relationship("UserToken", back_populates="user", uselist=False)
+    cv_feedbacks = relationship("CVFeedback", back_populates="user")
