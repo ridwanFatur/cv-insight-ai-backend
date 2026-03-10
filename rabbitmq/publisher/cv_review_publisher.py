5f4c62c7313dd3d1c009f030100f414dce8027cb
ridwanFatur <ridwan.faturrahnman.godjali@gmail.com>
2026-03-10 13:33:17 +0700

    Update

--- rabbitmq/publisher/cv_review_publisher.py
+++ rabbitmq/publisher/cv_review_publisher.py
@@ -6,14 +6,15 @@ from rabbitmq.constants import CV_REVIEW_TASKS
 from rabbitmq.channel_publisher import get_mq_channel_publisher
 
 
-def cv_review_publish(cv_feedback_id, user_id):
+def cv_review_publish(cv_feedback_id, user_id, file_link):
     channel = get_mq_channel_publisher()
     channel.basic_publish(
         exchange='',
         routing_key=CV_REVIEW_TASKS,
         body=json.dumps({
             "id": cv_feedback_id,
-            "user_id": user_id
+            "user_id": user_id,
+            "file_link": file_link,
         }),
         properties=pika.BasicProperties(
             delivery_mode=2,
